2bdf4f9e5d67bbf718c19e3f7c1f061c4a431540
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-09-21 10:49:21 -0700

    catch divide by 0 errors in funnel

--- macros/analyze_funnel.sql
+++ macros/analyze_funnel.sql
@@ -45,8 +45,9 @@ top_of_funnel as (
     where event_type = {{ "'" ~ event_funnel[0] ~ "'"}}
 ),
 
--- computes the overall (event / top of funnel event) and relative (event / previous step) dropoff for each event
-funnel as (
+-- computes the overall % dropoff (compared to the top of the funnel)
+-- selects the previous event's metrics to compute the relative funnel (next CTE)
+build_funnel as (
 
     select
         {{ 'grouped_events.' ~ group_by_column ~ "," if group_by_column != None }}
@@ -56,7 +57,7 @@ funnel as (
         grouped_events.number_of_events * 1.0 / top_of_funnel.init_number_of_events as overall_event_pct_dropoff, -- todo: this technically isn't "dropoff" (either subtract from 1 or call it something else)
         grouped_events.number_of_users * 1.0 / top_of_funnel.init_number_of_users as overall_user_pct_dropoff,
 
-        grouped_events.number_of_events * 1.0 / lag(grouped_events.number_of_events, 1) over (
+        lag(grouped_events.number_of_events, 1) over (
             -- only compare within groups
             {{ 'partition by grouped_events.' ~ group_by_column if group_by_column != None }} 
             -- match the given order
@@ -65,9 +66,9 @@ funnel as (
                 {%- for event_type in event_funnel %}
                 when {{ "'" ~ event_type ~ "'"}} then {{ loop.index }}
                 {% endfor %}
-                end ) as relative_event_pct_dropoff,
-        
-        grouped_events.number_of_users * 1.0 / lag(grouped_events.number_of_users, 1) over (
+                end ) as previous_step_number_of_events,
+
+        lag(grouped_events.number_of_users, 1) over (
             -- only compare within groups
             {{ 'partition by grouped_events.' ~ group_by_column if group_by_column != None }} 
             order by 
@@ -76,7 +77,7 @@ funnel as (
                 {%- for event_type in event_funnel %}
                 when {{ "'" ~ event_type ~ "'"}} then {{ loop.index }}
                 {% endfor %}
-                end ) as relative_user_pct_dropoff
+                end ) as previous_step_number_of_users
 
     from grouped_events
 
@@ -87,8 +88,35 @@ funnel as (
 
     {% else %} true {% endif -%}
 
+
+),
+
+-- returns the overall (event / top of funnel event) and relative (event / previous step) dropoff for each event
+funnel as (
+
+    select
+        {{ group_by_column ~ "," if group_by_column != None }}
+        event_type,
+        number_of_events,
+        number_of_users,
+
+        overall_event_pct_dropoff,
+        overall_user_pct_dropoff,
+
+        case 
+            when previous_step_number_of_events = 0 then 0 
+            when previous_step_number_of_events is null then 1 -- top of funnel
+            else number_of_events * 1.0 / previous_step_number_of_events end as relative_event_pct_dropoff,
+        
+        case 
+            when previous_step_number_of_users = 0 then 0 
+            when previous_step_number_of_users is null then 1
+            else number_of_users * 1.0 / previous_step_number_of_users end as relative_user_pct_dropoff
+
+    from build_funnel
+
     order by {{ group_by_column ~ "," if group_by_column != None }}
-        case grouped_events.event_type
+        case event_type
         {%- for event_type in event_funnel %}
         when {{ "'" ~ event_type ~ "'"}} then {{ loop.index }}
         {% endfor %}
