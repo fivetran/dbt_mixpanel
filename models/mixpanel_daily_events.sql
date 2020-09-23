ab877baf087aa2de01b5b0b449c32b7efc66ef15
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-09-23 14:58:19 -0700

    working on monthly events

--- models/mixpanel_daily_events.sql
+++ models/mixpanel_daily_events.sql
@@ -44,6 +44,7 @@ event_metrics as (
         count(distinct case when {{ dbt_utils.datediff('past_month.occurred_at', 'events.occurred_at', 'day') }} <= 7 then past_month.people_id end) as trailing_users_7d
         
     from events 
+    -- todo: can we use a window function instead of a self join?
     join events past_month 
         on events.event_type = past_month.event_type
         and {{ dbt_utils.datediff('past_month.occurred_at', 'events.occurred_at', 'day') }} <= 28
---
+++ models/mixpanel_monthly_events.sql
@@ -0,0 +1,77 @@
+-- probably want to config this as incremental 
+
+with events as (
+
+    select 
+        event_type,
+        occurred_at,
+        unique_event_id,
+        people_id
+    from {{ ref('mixpanel_event') }}
+
+    where {{ var('monthly_event_criteria', true) }} -- todo: should the monthly and daily tables use the same criteria?
+),
+
+user_monthly_events as (
+
+    select 
+        *, 
+        -- add window functions
+        min(date_month) over(partition by people_id, event_type) as first_month,
+        lag(people_id, 1) over(partition by people_id, event_type order by date_month asc) previous_month_with_event,
+        count(distinct people_id) over( partition by date_month ) as total_monthly_active_users
+
+    from (
+        -- use aggregate functions
+        select
+            people_id,
+            event_type,
+            {{ dbt_utils.date_trunc('month', 'events.occurred_at') }} as date_month,
+            count(unique_event_id) as number_of_events
+
+        from events
+        group by 1,2,3
+    )
+),
+
+monthly_metrics as (
+
+    select 
+        date_month,
+        event_type,
+        count(distinct people_id) as number_of_users,
+        count( distinct case when first_month = date_month then people_id end) as number_of_new_users,
+
+        count(distinct case when previous_month_with_event is not null and 
+            {{ dbt_utils.dateadd(datepart = 'month', interval = 1, from_date_or_timestamp = 'previous_month_with_event') }} = date_month 
+            then people_id end) as number_of_repeat_users,
+
+        count(distinct case when previous_month_with_event is not null and
+            {{ dbt_utils.datediff('previous_month_with_event', 'date_month', 'month') }} > 1
+            then people_id end) as number_of_return_users,
+
+        sum(number_of_events),
+        total_monthly_active_users
+
+
+    from user_monthly_events
+    group by 1,2
+),
+
+-- add churn!
+final as (
+
+    select
+        *,
+        -- subtract the returned users from the previous month's total users to get the # churned
+        -- note: churned users refer to users who did something last month and not this month
+        lag(number_of_users, 1) over(partition by date_month, event_type order by date_month asc) - number_of_repeat_users as number_of_churn_users,
+
+        lag(number_of_new_users, 1) over(partition by date_month, event_type order by date_month asc) - number_of_repeat_users as number_of_churned_new_users,
+        lag(number_of_repeat_users, 1) over(partition by date_month, event_type order by date_month asc) - number_of_repeat_users as number_of_churned_repeat_users,
+        lag(number_of_return_users, 1) over(partition by date_month, event_type order by date_month asc) - number_of_repeat_users as number_of_churned_return_users
+
+    from monthly_metrics
+)
+
+select * from final
