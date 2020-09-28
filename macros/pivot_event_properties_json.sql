d36b0ada147a8efb048c9c6afc3d548f2dce8a8f
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-09-28 11:01:26 -0700

    replace double quotes

--- macros/pivot_event_properties_json.sql
+++ macros/pivot_event_properties_json.sql
@@ -2,13 +2,13 @@
 {% for property in list_of_properties -%}
 
 {%- if target.type == 'bigquery' -%}
-json_extract(event_properties, {{ "'$." ~ property ~ "'" }} ) as {{ property }}
+replace(json_extract(event_properties, {{ "'$." ~ property ~ "'" }} ), '"', '') as {{ property }}
 
 {%- elif target.type == 'snowflake' -%}
-parse_json(event_properties):{{ property }} as {{ property }}
+replace(parse_json(event_properties):{{ property }}, '"', '') as {{ property }}
 
 {%- elif target.type == 'redshift' -%}
-json_extract_path_text(event_properties, {{ "'" ~ property ~ "'" }} ) as {{ property }}
+replace(json_extract_path_text(event_properties, {{ "'" ~ property ~ "'" }} ), '"', '') as {{ property }}
 {%- endif -%}
 
 {%- if not loop.last -%},{%- endif %}
