be7a6b85864940b23441610dcdbf7fe1c53e5aff
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-10-01 14:52:39 -0700

    make tmp model a simple view

--- models/staging/tmp/stg_mixpanel_event_tmp.sql
+++ models/staging/tmp/stg_mixpanel_event_tmp.sql
@@ -1,5 +1,8 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 select * 
 from {{ var('event_table' )}}
-
--- limit date range
-where time > {{ "'" ~ var('date_range_start',  '2010-01-01') ~ "'" }} 
