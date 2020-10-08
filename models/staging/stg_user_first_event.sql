488dc35e77f3749307243b1b1169eef641eab79b
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-10-08 12:19:09 -0700

    first event type table

---
+++ models/staging/stg_user_first_event.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+-- looking at events before we cut them off with a date range
+with alltime_events as (
+
+    select *
+    from {{ ref('stg_mixpanel_event') }}
+),
+
+first_events as (
+
+    select 
+        people_id,
+        event_type,
+        cast(min(date_day) as date) as first_event_day
+    
+    from alltime_events
+
+    group by people_id, event_type
+)
+
+select * from first_events
