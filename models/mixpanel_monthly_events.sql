2f2a2e25b3f252e8c77add489ac4571625390598
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-10-13 15:47:47 -0700

    redshift compatibility - can't do count distinct window function

--- models/mixpanel_monthly_events.sql
+++ models/mixpanel_monthly_events.sql
@@ -15,7 +15,8 @@ with events as (
         event_type,
         occurred_at,
         unique_event_id,
-        people_id
+        people_id,
+        cast( {{ dbt_utils.date_trunc('month', 'occurred_at') }} as date) as date_month
 
     from {{ ref('mixpanel_event') }}
 
@@ -31,21 +32,32 @@ with events as (
     {% endif %}
 ),
 
+month_totals as (
+    
+    select 
+        date_month,
+        count(distinct people_id) as total_monthly_active_users
+
+    from events
+
+    group by date_month
+),
+
 user_monthly_events as (
 
     select 
         *, 
         -- add window functions
         min(date_month) over(partition by people_id, event_type) as first_month,
-        lag(date_month, 1) over(partition by people_id, event_type order by date_month asc) previous_month_with_event,
-        count(distinct people_id) over( partition by date_month ) as total_monthly_active_users
+        lag(date_month, 1) over(partition by people_id, event_type order by date_month asc) previous_month_with_event
+        {# count(distinct people_id) over( partition by date_month ) as total_monthly_active_users #}
 
     from (
         -- use aggregate functions
         select
             people_id,
             event_type,
-            cast({{ dbt_utils.date_trunc('month', 'events.occurred_at') }} as date) as date_month,
+            date_month,
             count(unique_event_id) as number_of_events
 
         from events
@@ -56,27 +68,28 @@ user_monthly_events as (
 monthly_metrics as (
 
     select 
-        date_month,
-        event_type,
-        total_monthly_active_users,
-        count(distinct people_id) as number_of_users,
-        count( distinct case when first_month = date_month then people_id end) as number_of_new_users,
+        user_monthly_events.date_month,
+        user_monthly_events.event_type,
+        month_totals.total_monthly_active_users,
+        count(distinct user_monthly_events.people_id) as number_of_users,
+        count( distinct case when user_monthly_events.first_month = user_monthly_events.date_month then user_monthly_events.people_id end) as number_of_new_users,
 
         -- defining repeat user as someone who also performed this action the previous month
-        count(distinct case when previous_month_with_event is not null and 
-            {{ dbt_utils.datediff('previous_month_with_event', 'date_month', 'month') }} = 1
-            then people_id end) as number_of_repeat_users,
+        count(distinct case when user_monthly_events.previous_month_with_event is not null and 
+            {{ dbt_utils.datediff('user_monthly_events.previous_month_with_event', 'user_monthly_events.date_month', 'month') }} = 1
+            then user_monthly_events.people_id end) as number_of_repeat_users,
 
         -- defining return user as someone who has performed this action farther in the past
-        count(distinct case when previous_month_with_event is not null and
-            {{ dbt_utils.datediff('previous_month_with_event', 'date_month', 'month') }} > 1
-            then people_id end) as number_of_return_users,
+        count(distinct case when user_monthly_events.previous_month_with_event is not null and
+            {{ dbt_utils.datediff('user_monthly_events.previous_month_with_event', 'user_monthly_events.date_month', 'month') }} > 1
+            then user_monthly_events.people_id end) as number_of_return_users,
 
-        sum(number_of_events) as number_of_events
+        sum(user_monthly_events.number_of_events) as number_of_events
         
 
 
     from user_monthly_events
+        left join month_totals using(date_month)
     group by 1,2,3
 ),
 
@@ -88,7 +101,7 @@ final as (
 
         -- subtract the returned users from the previous month's total users to get the # churned
         -- note: churned users refer to users who did something last month and not this month
-        lag(number_of_users, 1) over(partition by event_type order by date_month asc) - number_of_repeat_users as number_of_churn_users,
+        coalesce(lag(number_of_users, 1) over(partition by event_type order by date_month asc) - number_of_repeat_users, 0) as number_of_churn_users,
 
         date_month || '-' || event_type as unique_key -- for incremental model :)
 
