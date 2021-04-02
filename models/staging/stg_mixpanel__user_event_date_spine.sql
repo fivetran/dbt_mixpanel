56e3c44f08d6ca51aac03ad7da70a761a035a194
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-04-02 13:48:23 -0700

    psotgres

--- models/staging/stg_mixpanel__user_event_date_spine.sql
+++ models/staging/stg_mixpanel__user_event_date_spine.sql
@@ -22,7 +22,7 @@ spine as (
     from (
         {{ dbt_utils.date_spine(
             datepart = "day", 
-            start_date =  "'" ~ var('date_range_start',  '2010-01-01') ~ "::date'" , 
+            start_date =  "'" ~ var('date_range_start',  '2010-01-01') ~ "'" ~ '::date' , 
             end_date = dbt_utils.dateadd("week", 1, dbt_utils.date_trunc('day', dbt_utils.current_timestamp())) 
             ) 
         }} 
