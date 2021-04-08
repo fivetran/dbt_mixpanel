01e3971072dff6778ff08c7e1bf0de6e0763b100
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-04-08 15:45:49 -0700

    adjust macros

--- models/staging/stg_mixpanel__event.sql
+++ models/staging/stg_mixpanel__event.sql
@@ -12,6 +12,7 @@ fields as (
     select
         cast( {{ dbt_utils.date_trunc('day', 'time') }} as date) as date_day,
         lower(name) as event_type,
+        cast(time as {{ dbt_utils.type_timestamp() }} )as occurred_at,
 
         -- pulls default properties and renames (see macros/staging_columns)
         -- columns missing from your source table will be completely NULL   
@@ -32,59 +33,6 @@ fields as (
 
     where {{ var('global_event_filter', 'true') }}
 
-),
-
--- aliasing happens in ../../macros/staging_columns.sql
--- writing out the below for recasting
-final as (
-
-    select
-        _fivetran_synced,
-        app_session_length,
-        app_build_number,
-        app_version,
-        has_bluetooth_enabled,
-        bluetooth_version,
-        device_brand,
-        browser,
-        browser_version,
-        wireless_carrier,
-        city,
-        current_url,
-        device_name,
-        device_id,
-        people_id,
-        people_id_before_identified,
-        google_play_service_status,
-        has_near_field_communication,
-        has_telephone,
-        initial_referrer,
-        initial_referring_domain,
-        insert_id,
-        mixpanel_library_version,
-        device_manufacturer,
-        device_model,
-        country_code,
-        referrer_keywords,
-        mixpanel_library,
-        mp_processing_time_ms,
-        event_type_original_casing,
-        os,
-        os_version,
-        event_properties,
-        network_type,
-        referrer,
-        referring_domain,
-        region,
-        screen_pixel_density,
-        screen_height,
-        screen_width,
-        search_engine,
-        cast(time as {{ dbt_utils.type_timestamp() }} )as occurred_at,
-        wifi as has_wifi_connected
-
-    from fields
-
 )
 
-select * from final
+select * from fields
