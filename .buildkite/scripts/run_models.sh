4504bd29fe2acfe5bb87f8485b9eb84b1b7149fa
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-07-23 10:16:48 -0500

    MagicBot/2024-q3-updates updates

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -16,9 +16,27 @@ db=$1
 echo `pwd`
 cd integration_tests
 dbt deps
+if [ "$db" = "databricks-sql" ]; then
+dbt seed --vars '{mixpanel_schema: mixpanel_sqlw_tests}' --target "$db" --full-refresh
+dbt compile --vars '{mixpanel_schema: mixpanel_sqlw_tests}' --target "$db"
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests}' --target "$db" --full-refresh
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests}' --target "$db"
+dbt test --vars '{mixpanel_schema: mixpanel_sqlw_tests}' --target "$db"
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests, fivetran_platform__usage_pricing: true}' --target "$db" --full-refresh
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests, fivetran_platform__usage_pricing: true}' --target "$db"
+dbt test --vars '{mixpanel_schema: mixpanel_sqlw_tests}' --target "$db"
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests, fivetran_platform__credits_pricing: false, fivetran_platform__usage_pricing: true}' --target "$db" --full-refresh
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests, fivetran_platform__credits_pricing: false, fivetran_platform__usage_pricing: true}' --target "$db"
+dbt test --vars '{mixpanel_schema: mixpanel_sqlw_tests}'  --target "$db"
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests, fivetran_platform__usage_pricing: false, fivetran_platform_using_destination_membership: false, fivetran_platform_using_user: false}' --target "$db" --full-refresh
+dbt run --vars '{mixpanel_schema: mixpanel_sqlw_tests, fivetran_platform__usage_pricing: false, fivetran_platform_using_destination_membership: false, fivetran_platform_using_user: false}' --target "$db"
+dbt test --vars '{mixpanel_schema: mixpanel_sqlw_tests}'  --target "$db"
+else
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 dbt run --target "$db"
 dbt test --target "$db"
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
+
+fi
