897147236477decbf7f17ac52fbae0e8629cfb7f
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-06-09 13:32:48 -0500

    fix tmp

--- models/staging/tmp/stg_mixpanel__event_tmp.sql
+++ models/staging/tmp/stg_mixpanel__event_tmp.sql
@@ -3,7 +3,7 @@
 
 {{
     fivetran_utils.union_connections(
-        connection_dictionary=var('mixpanel_sources'), 
+        connection_dictionary='mixpanel_sources',
         single_source_name='mixpanel', 
         single_table_name='event'
     )
